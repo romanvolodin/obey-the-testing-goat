1987f8fbca47c70548919f591e5a33d0fc973c33
Roman Volodin <roman.volodin@gmail.com>
2023-10-27 19:43:20 +0300

    Добавил первый юнит-тест, url path и фейковую вьюху

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import index_page
+
+
+class IndexPageTest(TestCase):
+    """Тест главной страницы"""
+
+    def test_root_url_resolves_to_index_page_view(self):
+        """Тест: корневой url обрабатывается правильной вьюхой"""
+        view = resolve("/")
+        self.assertEqual(view.func, index_page)
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index_page, name="index"),
 ]
