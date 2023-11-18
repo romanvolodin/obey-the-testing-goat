dffff2e43d80259ad6f7b194d44c74615c578c17
Roman Volodin <roman.volodin@gmail.com>
2023-11-18 11:51:06 +0300

    Index view теперь возвращает минимальный HTML

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import index_page
@@ -10,3 +11,13 @@ class IndexPageTest(TestCase):
         """Тест: корневой url обрабатывается правильной вьюхой"""
         view = resolve("/")
         self.assertEqual(view.func, index_page)
+
+    def test_home_page_returns_correct_html(self):
+        """Тест: домашняя страница возвращает правильный html"""
+        request = HttpRequest()
+        response = index_page(request)
+        html = response.content.decode("utf8")
+
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index_page():
-    return
+def index_page(request):
+    return HttpResponse("<html><title>To-Do lists</title></html>")
